a14fa6900cdae96c97a2c2c6e881f130af11cddd
Lux <64970263+ThunderSYX@users.noreply.github.com>
2021-11-25 10:36:10 -0500

    a

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -1,24 +1,41 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import world.GameMap;
+import world.TiledGameMap;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	OrthographicCamera cam;
+	GameMap gameMap;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		cam = new OrthographicCamera();
+		cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		gameMap = new TiledGameMap();
+
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		gameMap.render(cam);
 
+		if (Gdx.input.isTouched()){
+			cam.translate(-Gdx.input.getDeltaX(), Gdx.input.getDeltaY());
+			cam.update();
+		}
 	}
 	
 	@Override
