990c0d36be218876c6360f26a43f7afb0540ffc5
Lux <64970263+ThunderSYX@users.noreply.github.com>
2021-11-25 10:56:37 -0500

    Basic Mapbase
    Load maps, move around with mouse drag, click to get stats of block.

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -2,11 +2,14 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import world.GameMap;
+import world.TileType;
 import world.TiledGameMap;
 
 public class Main extends ApplicationAdapter {
@@ -30,17 +33,29 @@ public class Main extends ApplicationAdapter {
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		gameMap.render(cam);
 
 		if (Gdx.input.isTouched()){
 			cam.translate(-Gdx.input.getDeltaX(), Gdx.input.getDeltaY());
 			cam.update();
 		}
+
+		if (Gdx.input.justTouched()){
+			Vector3 pos = cam.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+			TileType type = gameMap.tileByLocation(0, pos.x, pos.y);
+
+			if (type != null){
+				System.out.println("Clicked on: " + type.getId() + " , " + type.getName() + " , " + type.isCollidable() + " , " + type.getDamage());
+			}
+		}
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		gameMap.dispose();
 	}
 }
